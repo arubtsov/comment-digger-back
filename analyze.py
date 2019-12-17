8899774bfad4829ddf5869e09e76c8d7f38ad863
arubtsov <arseniy.rubtsov@gmail.com>
2019-12-17 20:59:50 +0300

    extract filter function

--- analyze.py
+++ analyze.py
@@ -8,19 +8,16 @@ stop_words = set(stopwords.words("english"))
 def join_comments(comments):
     return ' '.join([comment['text'] for comment in comments])
 
-def remove_stop_words(words):    
-    return [word for word in words if word not in stop_words]
-
 def is_punctuation(word):
     return word in string.punctuation or all(char in string.punctuation for char in word)
 
-def remove_punctuation(words):
-    return [word for word in words if not is_punctuation(word)]
+def filter_words (tockens):
+    return [tocken for tocken in tockens if not is_punctuation(tocken) and tocken not in stop_words]
 
 def get_top_most_common(comments, number):
     text = join_comments(comments).lower()
-    tokenized_word = remove_punctuation(remove_stop_words(word_tokenize(text)))
-    frequency_distribution = FreqDist(tokenized_word)
+    filtered_words = filter_words(word_tokenize(text))
+    frequency_distribution = FreqDist(filtered_words)
     most_common = frequency_distribution.most_common(number)
 
     return [[word, freq] for word, freq in most_common]
